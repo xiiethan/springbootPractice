e781409c5f12de9aeb587a1ad2ff822d5e69c886
xiiethan <ethan_dillinger458@yahoo.com>
2023-02-07 22:21:56 +0000

    basic set up

---
+++ spring-boot-practice/spring-boot-practice/src/test/java/com/ethan/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
